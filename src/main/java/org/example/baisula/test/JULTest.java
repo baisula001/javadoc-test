143d0ae652dc83e0211f1cba88bca68fb99ffa7b
3575205331a@gmail.com <3575205331a@gmail.com>
2024-02-16 15:02:49 +0100

    final final

--- src/main/java/org/example/baisula/test/JULTest.java
+++ src/main/java/org/example/baisula/test/JULTest.java
@@ -4,18 +4,16 @@ import org.testng.annotations.Test;
 
 import java.util.logging.Logger;
 
-/**
- * @Auther: willson2008
- * @Date: 16/02/2024 - 02 - 16 - 13:44
- * @Description: org.example
- * @Version: 1.0
- */
+
 public class JULTest {
 
     @Test
     public void testQuick()throws Exception{
         Logger logger = Logger.getLogger("org.example.JULTest");
 
+        logger.info("hello JUL");
+        logger.info("hello JUL");
+        logger.info("hello JUL");
         logger.info("hello JUL");
         logger.info("hello JUL");
         logger.info("hello JUL");
